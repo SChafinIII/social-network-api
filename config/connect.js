22607a705a85221d876c143cc735ff1298a2f833
SChafinIII <118332995+SChafinIII@users.noreply.github.com>
2023-05-09 21:45:31 -0500

    Update connect.js

---
+++ config/connect.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB database.');
+});
+
+module.exports = db;
